bed384419c3f2c498521cde6fc3f02bfd5b46f95
Miguel Ángel Navarro <miguelanav01@gmail.com>
2025-10-05 22:24:26 +0200

    Update to Ejercicio 4

---
+++ ALUMNOS/MDAB/MIGUEL_NAVARRO/Practica_clase2.py
@@ -0,0 +1,32 @@
+# CLASE 2
+# 27/07/2025
+
+# print("Ejercicio 1:")
+# Imprime cada número de la lista multiplicado por el número
+# num = 7
+# listnum = [1, 2, 3, 4]
+# for contador in listnum:
+#     print(contador*num)
+
+
+# # Ejercicio 2:
+# # Imprime cada número del -10 al -1 con range
+# rane2 = range(-10, 0)
+# for contador in ran1:
+#     print(contador)
+
+
+# # Ejercicio 3:
+# # Imprime todos los números divisibles por 5 y por 7, dentro del rango (150, 350)
+# rane3 = range(150,351)
+# for contador in ran2:
+#     if contador % 5 == 0 and contador % 7 == 0:
+#         print(contador)
+
+
+# Ejercicio 4:
+# """
+# rane4 = range(0, 5)
+# while 
+#     print()
+# """
