8e87c2ab04172f2b47ee403bb605a4f499f8d5b8
Miguel Ángel Navarro <miguelanav01@gmail.com>
2025-10-07 19:38:00 +0200

    PYTHON Practica_clase2.py
    hasta el ejercicio 8

--- ALUMNOS/MDAB/MIGUEL_NAVARRO/Practica_clase2.py
+++ ALUMNOS/MDAB/MIGUEL_NAVARRO/Practica_clase2.py
@@ -1,27 +1,27 @@
 # CLASE 2
 # 27/07/2025
 
-# print("Ejercicio 1:") # Imprime cada número de la lista multiplicado por el número
+# print("EJERCICIO 1:") # Imprime cada número de la lista multiplicado por el número
 # num = 7
 # listnum = [1, 2, 3, 4]
 # for contador in listnum:
 #     print(contador*num)
 
 
-# print("Ejercicio 2:") # Imprime cada número del -10 al -1 con range
+# print("EJERCICIO 2:") # Imprime cada número del -10 al -1 con range
 # rane2 = range(-10, 0, 1)
 # for contador in rane2:
 #     print(contador)
 
 
-# print("Ejercicio 3:") # Imprime todos los números divisibles por 5 y por 7, dentro del rango (150, 350)
+# print("EJERCICIO 3:") # Imprime todos los números divisibles por 5 y por 7, dentro del rango (150, 350)
 # rane3 = range(150,351)
 # for contador in rane3:
 #     if contador % 5 == 0 and contador % 7 == 0:
 #         print(contador)
 
 
-# print("Ejercicio 4:") # Imprime un patrón de números por pantalla
+# print("EJERCICIO 4:") # Imprime un patrón de números por pantalla
 # rane4 = range(5, 0, -1)
 # for x in rane4: # "x" va a ser el mayor número del rango
 #     for y in range(x, 0, -1): # "y" es cada número (desde el mayor "x" hasta el último , 0)
@@ -29,7 +29,7 @@
 #     print() # salto de línea cuando X cambia
 
 
-# print("Ejercicio 5:") # Cuenta cuántos nºs especiales (múltiplo de 3 o contiene el dígito "3") hay en una lista
+# print("EJERCICIO 5:") # Cuenta cuántos nºs especiales (múltiplo de 3 o contiene el dígito "3") hay en una lista
 # listanum = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15]
 # esp_cont: int = 0 # empiezo con el contador de nºs especiales en 0
 # esp_list = []
@@ -53,4 +53,55 @@
 # cuenta_atras(12)
 
 
-print("EJERCICIO 7:")
+# print("EJERCICIO 7:")
+# string = "Ole Python como mola! Me está encantando. De verdad que SÍ"
+# cont_mayus = 0
+# cont_minus = 0
+# cont_dig = 0
+# for caracter in string:
+#     if caracter.isupper():
+#         cont_mayus += 1
+# for caracter in string:
+#     if caracter.islower():
+#         cont_minus += 1
+# for caracter in string:
+#     if caracter.isdigit():
+#         cont_dig += 1
+# print(f"En la string '{string}'")
+# print(f"Hay {cont_mayus} letras en mayúscula, {cont_minus} letras en minúscula y {cont_dig} dígitos en total")
+
+
+# print("EJERCICIO 8:")
+# dias_semana: dict = {
+#     1: "lunes",
+#     2: "martes",
+#     3: "miércoles",
+#     4: "jueves",
+#     5: "viernes",
+#     6: "sábado",
+#     7: "domingo"
+# }
+# def semanal_1(dia):
+#     print(dias_semana[dia])
+# semanal_1(5)
+# def semanal_2(dia):
+#     match dia:
+#         case 1:
+#             print("lunes")
+#         case 2:
+#             print("martes")
+#         case 3:
+#             print("miércoles")
+#         case 4:
+#             print("jueves")
+#         case 5:
+#             print("viernes")
+#         case 6:
+#             print("sábado")
+#         case 7:
+#             print("domingo")
+#         case _:
+#             print("Error")
+# semanal_2(6)
+
+
