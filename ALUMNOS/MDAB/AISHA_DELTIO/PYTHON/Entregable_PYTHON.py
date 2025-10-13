376562ecc94eb52df70d67c77735c164d13529e2
Aisha del Tio <adeltiodeprado@gmail.com>
2025-10-13 20:05:28 +0200

    Update Entregable_PYTHON.oy
    1.Realizacion ejercicios 1,2,3
    2.Importante: encontrarse en la carpeta para poder ejecutar el código (he necesitado hacer cd ; preguntar si existe alguna forma más automatica)

--- ALUMNOS/MDAB/AISHA_DELTIO/PYTHON/Entregable_PYTHON.py
+++ ALUMNOS/MDAB/AISHA_DELTIO/PYTHON/Entregable_PYTHON.py
@@ -1,6 +1,6 @@
 # # EJERCICIOS ENTREGABLES : PYTHON
 # # AUTOR: AISHA DEL TIO DE PRADO
-# terminal: py Entregable_PYTHON_AISHA_DELTIO.py
+# terminal: py Entregable_PYTHON.py
 
 # # EJERCICIO 1:
 
@@ -15,4 +15,40 @@ print(f'El término "Marina de Empresas 2025" tiene una longitud de {longitud_ed
 
 primera_edem = edem[0]
 print(f'La primera letra de "Marina de Empresas 2025" es {primera_edem} ')
+
+
+
+# # EJERCICIO 2:
+
+# Define una variable festivo que sea de tipo booleano.
+# Crea una condición en la que sí festivo es verdadero se muestre por consola “Hoy es fiesta voy a echarme una siesta!!” 
+# y sino que muestre por consola “No es fiesta pero no pasa nada porque tengo que hacer el entregable de Python :) ”
+
+festivo = True
+
+if festivo == True:
+    print('Hoy es fiesta voy a echarme una siesta!!')
+else:
+    print('No es fiesta pero no pasa nada porque tengo que hacer el entregable de Python :)')
+
+
+
+# # EJERCICIO 3:
+
+# Crea la función ultimoCaracter debe recibir un tipo string y devolver un string con el último carácter.
+# Si la función no recibe un dato tipo string debe devolver el string 'Debo ser ejecutada con un string'.    
+
+def ultimoCaracter(palabra):
+    if type(palabra) == str:
+        ultimo_crc = palabra[-1]
+        return f'El ultimo caracter de la palabra {palabra} es : {ultimo_crc}'
+    else:
+        return f'Debo ser ejecutada con un string'
+        
+print(ultimoCaracter("Aisha"))
+print(ultimoCaracter(7))    
+    
+    
+
+# # EJERCICIO 4:
 
