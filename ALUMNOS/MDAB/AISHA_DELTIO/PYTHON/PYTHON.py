10f2da0e455f27075621b68d4ba85298e03a14be
Aisha del Tio <adeltiodeprado@gmail.com>
2025-10-13 19:41:43 +0200

    Create PYTHON.py
    Creamos el archivo para realizar la tarea

---
+++ ALUMNOS/MDAB/AISHA_DELTIO/PYTHON/PYTHON.py
@@ -0,0 +1,18 @@
+# # EJERCICIOS ENTREGABLES : PYTHON
+# # AUTOR: AISHA DEL TIO DE PRADO
+# terminal: py Entregable_PYTHON_AISHA_DELTIO.py
+
+# # EJERCICIO 1:
+
+# Se debe trabajar con una variable que contiene la información: “Marina de Empresas 2025”
+# Muestra por consola la longitud de la variable
+# Utilizando esa variable muestra por consola la primera letra.
+
+edem = "Marina de Empresas 2025"
+
+longitud_edem = len(edem)
+print(f'El término "Marina de Empresas 2025" tiene una longitud de {longitud_edem} ')
+
+primera_edem = edem[0]
+print(f'La primera letra de "Marina de Empresas 2025" es {primera_edem} ')
+
