6b600698daace2e79d73741b3c1c8a7df0ac3dec
Elena Morales <elenamoralesp7@gmail.com>
2025-10-14 11:52:39 +0200

    commit

--- ALUMNOS/MDAB/ELENA_MORALES/PYTHON/entregable_python_elena_morales.py.py
+++
@@ -1,6 +0,0 @@
-# ENTREGABLE PYTHON
-
-# EJERCICIO 1
-
-texto = "Marina de Empresa 2025"
-print(len(text))
---
+++ ALUMNOS/MDAB/ELENA_MORALES/PYTHON/scriptem.py
@@ -0,0 +1,7 @@
+# ENTREGABLE PYTHON
+
+# EJERCICIO 1
+
+texto = "Marina de Empresa 2025"
+print(f"Longitud: ", len(texto), "palabras")
+print(f"Primera letra: ", texto[0])
