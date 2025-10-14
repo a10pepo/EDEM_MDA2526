57d33a004b9a7f79f8647acc55ef0d7d7b633fee
Aisha del Tio <adeltiodeprado@gmail.com>
2025-10-14 17:20:24 +0200

    Update Entregable_PYTHON
    Finalizacion del ejercicio 4 y revisión del resto de los ejercicios

--- ALUMNOS/MDAB/AISHA_DELTIO/PYTHON/Entregable_PYTHON.py
+++ ALUMNOS/MDAB/AISHA_DELTIO/PYTHON/Entregable_PYTHON.py
@@ -1,9 +1,8 @@
 # # EJERCICIOS ENTREGABLES : PYTHON
 # # AUTOR: AISHA DEL TIO DE PRADO
-# terminal: py Entregable_PYTHON.py
+
 
 # # EJERCICIO 1:
-
 # Se debe trabajar con una variable que contiene la información: “Marina de Empresas 2025”
 # Muestra por consola la longitud de la variable
 # Utilizando esa variable muestra por consola la primera letra.
@@ -19,7 +18,6 @@ print(f'La primera letra de "Marina de Empresas 2025" es {primera_edem} ')
 
 
 # # EJERCICIO 2:
-
 # Define una variable festivo que sea de tipo booleano.
 # Crea una condición en la que sí festivo es verdadero se muestre por consola “Hoy es fiesta voy a echarme una siesta!!” 
 # y sino que muestre por consola “No es fiesta pero no pasa nada porque tengo que hacer el entregable de Python :) ”
@@ -34,7 +32,6 @@ else:
 
 
 # # EJERCICIO 3:
-
 # Crea la función ultimoCaracter debe recibir un tipo string y devolver un string con el último carácter.
 # Si la función no recibe un dato tipo string debe devolver el string 'Debo ser ejecutada con un string'.    
 
@@ -51,4 +48,38 @@ print(ultimoCaracter(7))
     
 
 # # EJERCICIO 4:
+# Escribe un programa que pida una palabra por teclado y diga si está en la lista de palabras prohibidas (bad_words.txt). 
+# La comparación debe ser insensible a mayúsculas .
 
+
+# Normalizacion: convierte a minúsculas y quita espacios al inicio y al final
+def norm(s: str):
+    return s.lower().strip()
+
+# Codigo profesora
+bad = set()
+with open("bad_words.txt", encoding="utf-8") as f: # abrimos el archivo
+    for line in f: # recorremos el archivo linea por linea
+        w = line.strip() # eliminamos espacios en blanco y saltos de línea al principio y al final
+        if w:
+            bad.add(norm(w))
+
+# Entrada: pide al usuario una sola palabra 
+palabra = input('Escribe una única palabra: ')
+
+# Validacion: Si la entrada está vacía o contiene espacios termina el programa
+if palabra == '' or ' ' in palabra:
+    print(f'Introduce una sola palabra (sin espacios).')
+else:
+    if norm(palabra) in bad: # normalizacion + comrpobacion 
+        print('NO CORRECTA') # si pertenece, la palabra no es correcta
+    else:
+        print('CORRECTA') # si no pertenece, la palabra es correcta
+
+    
+
+
+            
+            
+            
+            
