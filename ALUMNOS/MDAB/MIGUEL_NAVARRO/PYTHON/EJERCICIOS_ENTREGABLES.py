f10f03a04f0f45871f17cab214d5f12d97cf3b7a
Miguel Ángel Navarro <miguelanav01@gmail.com>
2025-10-19 19:14:05 +0200

    Last_exercise solved

--- ALUMNOS/MDAB/MIGUEL_NAVARRO/PYTHON/EJERCICIOS_ENTREGABLES.py
+++ ALUMNOS/MDAB/MIGUEL_NAVARRO/PYTHON/EJERCICIOS_ENTREGABLES.py
@@ -1,58 +1,57 @@
-"""
-EJERCICIOS ENTREGABLES - PYTHON
-(1)
-"""
+print("EJERCICIOS ENTREGABLES - PYTHON (1)")
 
-"""
-print("Ejercicio 1")
-var1 = "Marina de Empresas 2025"
-print(f"La variable '{var1}' tiene {len(var1)} caracteres.")
-print(f"La primera letra es la {var1[0]}")
+
+print("\nEjercicio 1")
+var1 = "Marina de Empresas 2025"    # defino la variable string
+print(f"La variable '{var1}' tiene {len(var1)} caracteres.")    # longitud de la variable
+print(f"La primera letra es la '{var1[0]}'")    # primera letra
+
 
 print("\nEjercicio 2")
-festivo = False
+festivo = False # variable "festivo" de tipo Booleano
 if festivo == True:
     print("Hoy es fiesta, voy a echarme una siesta!!")
 else:
     print("No es fiesta pero no pasa nada porque tengo que hacer el entregable de Python :)")
 
+
 print("\nEjercicio 3")
 def ultimoCaracter(cadena: str):
-    if not isinstance(cadena, str):
-        print("Debo ser ejecutada con un string")
-    else:
+    """
+    Recibe un string y devuelve el último carácter de la misma en un nuevo string
+    Args: string
+    Returns: string: última letra del string recibido
+    """
+    if isinstance(cadena, str):
         print(f"'{cadena[-1]}' es el último carácter")
-ultimoCaracter("Python")
-"""
-"""
-print("\nEjercicio 4")
+    else:
+        print("Debo ser ejecutada con un string")
+ultimoCaracter("Python")    # llamo a la función
 
-ad = set()
+
+print("\nEjercicio 4 - Bad Words")
+
+def norm(s: str):
+    """
+    Convierte un string a minúsculas y elimina los espacios al inicio y al final de la misma
+    Args: string
+    Returns: string en minúsculas y sin espacios al inicio y al final
+    """
+    return s.strip().lower()
+
+bad = set()         # guarda el bloque que carga "bad_words.txt" en un set llamado bad con una palabra por línea
 with open("bad_words.txt", encoding="utf-8") as f:
     for line in f:
         w = line.strip()
         if w:
             bad.add(norm(w))
 
-palabra = input("Introduzca una palabra: ")
-print(f"La palabra es {palabra}")
-"""
-
-
-badword = ["word1", "word2", "word3", "word4"]
-word_wanted = "word3"
-for word in badword:
-    print(f"Checking word: {word}")
-    if word == word_wanted:
-        print(f"Word found! {word} is on the list")
-        break
-    print(f"The word '{word}' is not on the list")
-print("Ended program")
-
-numeros = [1, 2, 3, 4, 5, 6, 7]
-for num in numeros:
-    if num % 2 == 0:
-        print(f"Saltando el número par: {num}")
-        continue
-    print(f"Procesando número impar: {num} (su cuadrado es {num * num})")
-print("Fin del programa tras el bucle")
+word = input("Introduce una sola palabra. ")    # Pide una palabra
+if (" " in word) or (word == ""):   # Comprueba que la entrada no está vacía no tiene espacios
+    print("Introduce una sola palabra (sin espacios).")
+else:
+    word = norm(word)               # Normaliza la palabra introducida
+    if word in bad:                 # Comprueba si pertenece al conjutno bad
+        print("NO CORRECTA")
+    else:
+        print("CORRECTA")
