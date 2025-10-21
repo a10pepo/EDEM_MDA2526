af86ebe1d2260bc2c17342232248490c42360601
SalvillaT <salvador.reche.vazquez@gmail.com>
2025-10-21 18:56:56 +0200

    Commit del entregable de python

---
+++ ALUMNOS/MDAA/SALVADOR_RECHE/PYTHON/ejercicio2.py
@@ -0,0 +1,6 @@
+festivo = input("Dime que dia de la semana es hoy: ").lower()
+
+if festivo == "sabado":
+    print("Hoy es fiesta voy a echarme una siesta!!")
+else:
+    print("No es fiesta pero no pasa nada porque tengo que hacer el entregable de Python :)")
