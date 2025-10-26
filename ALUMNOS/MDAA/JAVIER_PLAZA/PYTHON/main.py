35ac6236d6efa711663776f769f9a8b793b5d795
JaviPlazaRosique <j.plazarosique@gmail.com>
2025-10-26 13:03:41 +0100

    creacion carpeta python y resolucion ejercicio 1 y 2

---
+++ ALUMNOS/MDAA/JAVIER_PLAZA/PYTHON/main.py
@@ -0,0 +1,16 @@
+# Ejercicio 1
+
+mde = "Marina de Empresas 2025"
+
+print(len(mde))
+
+print(mde[0])
+
+# Ejercicio 2
+
+festivo = False
+
+if festivo == True:
+    print("Hoy es fiesta voy a echarme una siesta!!")
+else:
+    print("No es fiesta pero no pasa nada porque tengo que hacer el entregable de Python :)")
