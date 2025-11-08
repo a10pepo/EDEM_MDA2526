8c1dddb33457c89cc9cc654299d4f43357d505c9
gemmabalagueer <gbalagueradell@gmail.com>
2025-11-08 15:49:09 +0100

    cambios

--- ALUMNOS/MDAA/GEMMA_BALAGUER/SQL/main.py
+++ ALUMNOS/MDAA/GEMMA_BALAGUER/SQL/main.py
@@ -1,18 +1,41 @@
-import psycopg
+import os, psycopg
 
-# Conexión directa
-connection = psycopg.connect("postgresql://postgres:postgres@localhost:5433/pruebadb")
+#URL CONEXIÓN A BD 
+url = os.getenv("DATABASE_URL")
+#CONEXIÓN A BD
+connection = psycopg.connect(url)
+# Cursor
+try:
+    cur = connection.cursor()
+    print("BD conectada con éxito")
+except psycopg.Error as e:
+    print("Error conectando a la BD:", e)
 
-cur = connection.cursor()
-print("BD conectada con éxito")
 
-
-def getUsers():
-    query = "SELECT * FROM users;"
+def getemployees(cur):
+    query = "SELECT * FROM employees;"
     cur.execute(query)
-    print("Nuestros usuarios:",cur.fetchall())
+    print("Nuestros empleados:",cur.fetchall())
+
+getemployees(cur)
+
+
+def getdepartments(cur):
+    query = "SELECT * FROM departments;"
+    cur.execute(query)
+    print("Nuestros deartamentos:",cur.fetchall())
+
+getdepartments(cur)
+
+def getEmployeesWithDepartments(cur):
+    query = "SELECT * FROM employees INNER JOIN departments ON departments.id = employees.departments_id;"
+    cur.execute(query)
+    print("Estos son los empleados que tienen un departamento:", cur.fetchall())
+
+
+getEmployeesWithDepartments(cur)
 
 
 
-getUsers()
+
 
