1a86bd6beea3d82232c3d0d93844345da2cef354
Miguel Ángel Navarro <miguelanav01@gmail.com>
2025-11-08 11:33:37 +0100

    Update ahorcado

--- ALUMNOS/MDAB/MIGUEL_NAVARRO/AHORCADO/ahorcado.py
+++ ALUMNOS/MDAB/MIGUEL_NAVARRO/AHORCADO/ahorcado.py
@@ -1,33 +1,42 @@
-import string
+import string, sys, os, psycopg
 
-# Leo el fichero y lo guardo en words (list)
-with open("palabras.txt", 'r') as archive:
-    content = archive.read()
+# URL CONEXIÓN A BD
+url = os.getenv("DATABASE_URL")
+# CONEXIÓN A BD
+connection = psycopg.connect(url)
+# Cursor
+cur = connection.cursor()
+print("BD conectada con éxito")
+
+# Leo el fichero y lo guardo
+with open(sys.argv[1], 'r') as f:
+    content = f.read()
     words = content.split()
-# Guardo el abecedario en letters (string)
-letters = string.ascii_uppercase
+letters = string.ascii_uppercase    # Guardo el abecedario
 
+# Número de intentos en el ahorcado
 total_strikes = 0
-
 for word in words:          # cada palabra
     known_letters = 0
     for letter in letters:  # cada letra
         total_strikes += 1
-        if letter in word:  # letra en la palabra
-            print(letter, " in ", word)
-            known_letters += word.count(letter)
+        if letter in word:  # letra en la palabra ?
+            known_letters += word.count(letter) # letras adivinadas
         if known_letters == len(word):
             break
     
-
 print("Total strikes: ", total_strikes)
-print(len(letters))
 
 
 
-
-                
-
+cur.execute("""CREATE TABLE IF NOT EXISTS ahorcado(
+            id                  INTEGER GENERATED ALWAYS AS INDENTITY PRIMARY KEY,
+            palabra             VARCHAR(30) NOT NULL,
+            letras_acertadas    VARCHAR(30),
+            letras_falladas     VARCHAR(30),
+            intentos            INTEGER,
+            tiempo              TIMESTAMPZ DEFAULT NOW()
+            );""")
 
 
 
