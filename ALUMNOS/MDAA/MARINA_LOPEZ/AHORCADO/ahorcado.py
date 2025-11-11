af391bc6501eee2afcebbe826bd2c81970d03add
marina-a-lopez <malope@edem.es>
2025-11-11 12:32:26 +0100

    Correccion ahorcado
    se realizan varios intentos por si el contendor arranca antes de la base de datos

--- ALUMNOS/MDAA/MARINA_LOPEZ/AHORCADO/ahorcado.py
+++ ALUMNOS/MDAA/MARINA_LOPEZ/AHORCADO/ahorcado.py
@@ -1,29 +1,44 @@
-import os, psycopg, requests
-try:
-     #URL CONEXIÓN A BD 
-     url = os.getenv("DATABASE_URL")
-     #CONEXIÓN A BD
-     connection = psycopg.connect(url)
-     # Cursor
-     cur = connection.cursor()
-     print("BD conectada con éxito")
-except:
-     print("Error conectando a la BD")
+import os
+import psycopg
+import requests
+import time
 
+connection = None
+cur = None 
 
-def createIntentos ():
-    query = """CREATE TABLE IF NOT EXISTS Intentos(
-id INTEGER GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-palabra TEXT,
-letras_acertadas TEXT,
-letras_falladas TEXT,
-intentos INTEGER,
-tiempo TIMESTAMPTZ NOT NULL DEFAULT NOW()
-);"""
-    cur.execute(query)
-    print("Tabla creada con exito")
-createIntentos()
-connection.commit()
+for intento in range(10):
+    try:
+        print(f"Intento de conexión {intento + 1}/10")
+        #URL CONEXIÓN A BD 
+        url = os.getenv("DATABASE_URL")
+        #CONEXIÓN A BD
+        connection = psycopg.connect(url)
+        # Cursor
+        cur = connection.cursor()
+        print("BD conectada con éxito")
+        break
+    except Exception as e:
+        print(f"Error conectando a la BD: {e}")
+        if intento < 9:
+            print("Reintentando en 5 segundos...")
+            time.sleep(5)
+        else:
+            print("No se pudo conectar a la BD después de 10 intentos.")
+
+if connection and cur:
+    def createIntentos ():
+        query = """CREATE TABLE IF NOT EXISTS Intentos(
+    id INTEGER GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    palabra TEXT,
+    letras_acertadas TEXT,
+    letras_falladas TEXT,
+    intentos INTEGER,
+    tiempo TIMESTAMPTZ NOT NULL DEFAULT NOW()
+    );"""
+        cur.execute(query)
+        print("Tabla creada con exito")
+    createIntentos()
+    connection.commit()
 
 
 
@@ -55,5 +70,4 @@ def ahorcado():
                 connection.commit()
     print(f"El total de intentos para todas las palabras: {total}")
 ahorcado()
-
 
