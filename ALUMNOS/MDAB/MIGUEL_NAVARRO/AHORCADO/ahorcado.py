0930db5188cb5a4aaf861f74278deb8957257e42
Miguel Ángel Navarro <miguelanav01@gmail.com>
2025-11-18 17:32:17 +0100

    Advance

--- ALUMNOS/MDAB/MIGUEL_NAVARRO/AHORCADO/ahorcado.py
+++ ALUMNOS/MDAB/MIGUEL_NAVARRO/AHORCADO/ahorcado.py
@@ -1,4 +1,4 @@
-import string, sys, os, psycopg, time, requests
+import string, sys, os, psycopg, time, requests, json
 
 # URL CONEXIÓN A BD
 url = os.getenv("DATABASE_URL")
@@ -66,28 +66,36 @@ connection.commit()
 print("Intentos totales: ", total_strikes)
 """
 
+try:
+    url = "https://rae-api.com/api/random"
+    headers = {"Accept": "application/json"}
+    response = requests.get(url, headers=headers)
+    data = response.json()
+except requests.exceptions.RequestException as e:
+    print("Error accessing to url:", e)
 
-total_strikes = 0                # Número de intentos totales en el ahorcado
+print(data["data"]["word"], "es la palabra")
+palabra = "cara"
+print(palabra, "tiene", {len(palabra)}, "letras")
 
-for palabra in words:       # cada palabra
-    letras_acertadas = ""   # string vacía
-    letras_falladas = ""    # string vacía
-    intentos = 0            # Número de intentos para esta palabra
-    known_letters = 0
-    for letra in letters:   # cada letra
-        intentos += 1
-        total_strikes +=1
-        if letra in palabra:    # letra en la palabra?
-            known_letters += palabra.count(letra)   # nº de letras adivinadas
-            letras_acertadas += letra
-        else:
-            letras_falladas += letra
-        insertAhorcado()
-        if known_letters == len(palabra):
-            break
-    print(f"{palabra} - {intentos} intentos")
+intentos = 0            # Número de intentos para esta palabra
+letras_acertadas = ""   # string vacía
+letras_falladas = ""    # string vacía
+known_letters = 0
+
+for letra in letters:   # cada letra
+    intentos += 1
+    if letra in palabra:    # letra en la palabra?
+        known_letters += palabra.count(letra)   # nº de letras adivinadas
+        letras_acertadas += letra
+        print(letra, letras_acertadas)
+    else:
+        letras_falladas += letra
+    # insertAhorcado()
+    if known_letters == len(palabra):
+        break
+print(f"{palabra} - {intentos} intentos")
 connection.commit()
-print("Intentos totales: ", total_strikes)
 
 # cur.close()
 # connection.close()
--- ALUMNOS/MDAB/MIGUEL_NAVARRO/AHORCADO/pruebas.py
+++ ALUMNOS/MDAB/MIGUEL_NAVARRO/AHORCADO/pruebas.py
@@ -1,26 +1,22 @@
-import string
+letters = "EAOSRNIDLCTUMPBGVYQHFZJXKW"      # Ordenador por frecuencia general españo
 
-words = ["MURCIELAGO", "VIAJE", "EVADIR", "ZAPATO", "CIELO", "RECREO", "PIZARRA", "MATEMATICAS", "PROGRAMACION", "ORDENADOR"]
-# letters = string.ascii_uppercase    # Guardo el abecedario
-# letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"    # Abecedario manual
-# letters = "AEIOUBCDFGHJKLMNPQRSTVWXYZ"
-# letters = "EAOSRNIDLCTUMPBGVYQHFZJXKW"
+palabra = "cara"
+print(palabra, "tiene", {len(palabra)}, "letras")
 
-total_strikes = 0                # Número de intentos totales en el ahorcado
-for palabra in words:       # cada palabra
-    letras_acertadas = ""   # string vacía
-    letras_falladas = ""    # string vacía
-    intentos = 0            # Número de intentos para esta palabra
-    known_letters = 0
-    for letra in letters:   # cada letra
-        intentos += 1
-        total_strikes +=1
-        if letra in palabra:    # letra en la palabra?
-            known_letters += palabra.count(letra)   # nº de letras adivinadas
-            letras_acertadas += letra
-        else:
-            letras_falladas += letra
-        if known_letters == len(palabra):
-            break
-    print(f"{intentos}")
-print("Total =", total_strikes)
+intentos = 0            # Número de intentos para esta palabra
+letras_acertadas = ""   # string vacía
+letras_falladas = ""    # string vacía
+known_letters = 0
+
+for letra in letters:   # cada letra
+    intentos += 1
+    if letra in palabra:    # letra en la palabra?
+        known_letters += palabra.count(letra)   # nº de letras adivinadas
+        letras_acertadas += letra
+        print(letra, letras_acertadas)
+    else:
+        letras_falladas += letra
+    # insertAhorcado()
+    if known_letters == len(palabra):
+        break
+print(f"{palabra} - {intentos} intentos")
