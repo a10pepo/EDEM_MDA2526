1f91bf4d2aa459458fa151373cf9e316a068b830
Javier Plaza Rosique <j.plazarosique@gmail.com>
2025-11-20 19:06:18 +0100

    cambios .py

--- ALUMNOS/MDAA/JAVIER_PLAZA/VALENCIA/valencia.py
+++ ALUMNOS/MDAA/JAVIER_PLAZA/VALENCIA/valencia.py
@@ -1,14 +1,62 @@
 import requests 
 import psycopg
+import os
+import time
 
 respuesta = requests.get("https://valencia.opendatasoft.com/api/explore/v2.1/catalog/datasets/valenbisi-disponibilitat-valenbisi-dsiponibilidad/records?limit=20")
 
 respuesta = respuesta.json()
 
-resultados = []
+
+def crear_tabla():
+    try:
+        cur.execute("""
+        CREATE TABLE valenbisi_raw (
+        id SERIAL PRIMARY KEY,
+        station_id INTEGER NOT NULL,
+        station_name VARCHAR(255),
+        latitude DECIMAL(10, 8),
+        longitude DECIMAL(11, 8),
+        available_bikes INTEGER,
+        available_slots INTEGER,
+        station_status VARCHAR(50),
+        total_capacity INTEGER,
+        timestamp TIMESTAMP NOT NULL
+        ); """)
+        cur.close()
+        connection.commit()
+    except Exception as e:
+        print("Error al crear la tabla:", e)
+
+def añadirDatos(station_id, station_name, latitude, longitude, available_bikes, available_slots, station_status, total_capacity, timestamp):
+    try:
+        query = """INSERT INTO valenbisi_raw(station_id, station_name, latitude, longitude, available_bikes, available_slots, station_status, total_capacity, timestamp)
+        VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s)"""
+        cur.execute(query, (station_id, station_name, latitude, longitude, available_bikes, available_slots, station_status, total_capacity, timestamp))
+        connection.commit()
+        print('Datos añadidos')
+    except Exception as e:
+        print("Error:", e)
+
+try: 
+    url = os.getenv("DATABASE_URL")
+    connection = psycopg.connect(url)
+    cur = connection.cursor()
+    print('Base de datos conectada con exito')
+except Exception as e:
+    print('Error', e)
+
+crear_tabla()
+
 for resultado in respuesta["results"]:
-    print(resultado)
-
-
-
+    for station_id in resultado["number"]:
+        for station_name in resultado["address"]:
+            for latitude in resultado["geo_point_2d"]["lat"]:
+                for longitude in resultado["geo_point_2d"]["lon"]:
+                    for available_bikes in resultado["available"]:
+                        for available_slots in resultado["free"]:
+                            for station_status in resultado["open"]:
+                                for total_capacity in resultado["total"]:
+                                    for timestamp in resultado["update_jcd"]:
+                                        añadirDatos(station_id, station_name, latitude, longitude, available_bikes, available_slots, station_status, total_capacity, timestamp)
 
