2f71394f4feb547f61dd15bb470dd48ac8877655
Javier Plaza Rosique <j.plazarosique@gmail.com>
2025-11-20 18:14:43 +0100

    actualizacion

--- ALUMNOS/MDAA/JAVIER_PLAZA/VALENCIA/valencia.py
+++ ALUMNOS/MDAA/JAVIER_PLAZA/VALENCIA/valencia.py
@@ -1,9 +1,14 @@
 import requests 
 import psycopg
 
-respuesta = requests.get("https://valencia.opendatasoft.com/api/records/1.0/search/?dataset=valenbisi-disponibilitat-valenbisi-dsiponibilidad")
+respuesta = requests.get("https://valencia.opendatasoft.com/api/explore/v2.1/catalog/datasets/valenbisi-disponibilitat-valenbisi-dsiponibilidad/records?limit=20")
 
 respuesta = respuesta.json()
 
-print(respuesta)
+resultados = []
+for resultado in respuesta["results"]:
+    print(resultado)
+
+
+
 
