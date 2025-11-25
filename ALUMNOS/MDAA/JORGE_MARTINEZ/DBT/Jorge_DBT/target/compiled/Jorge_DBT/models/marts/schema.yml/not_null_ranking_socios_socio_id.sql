76d22c97562421ebcfac632d5d176bfc5dab7e31
Jorgemart9 <jorgemart9.jm@gmail.com>
2025-11-25 15:16:17 +0100

    DBT_Entregable_Jorge_Martinez

---
+++ ALUMNOS/MDAA/JORGE_MARTINEZ/DBT/Jorge_DBT/target/compiled/Jorge_DBT/models/marts/schema.yml/not_null_ranking_socios_socio_id.sql
@@ -0,0 +1,11 @@
+
+    
+    
+
+
+
+select socio_id
+from "dev"."main"."ranking_socios"
+where socio_id is null
+
+
---
+++ ALUMNOS/MDAA/JORGE_MARTINEZ/DBT/Jorge_DBT/target/run/Jorge_DBT/models/staging/stg_ventas.sql
@@ -0,0 +1,18 @@
+
+  
+  create view "dev"."main"."stg_ventas__dbt_tmp" as (
+    with source as (
+    select * from "dev"."main"."raw_ventas"
+),
+renamed as (
+    select
+        id_ticket as venta_id,
+        id_socio as socio_id,
+        id_producto as producto_id,
+        fecha_compra,
+        cantidad,
+        estado
+    from source
+)
+select * from renamed
+  );
