95ae44bf4803170b12e6143e8445b0b3b46994ac
Miguel Ángel Navarro <miguelanav01@gmail.com>
2025-12-08 20:06:45 +0100

    DBT Project Miguel Navarro

--- ALUMNOS/MDAB/MIGUEL_NAVARRO/DBT/mygoal/models/marts/mart__player_analysis.sql
+++ ALUMNOS/MDAB/MIGUEL_NAVARRO/DBT/mygoal/models/marts/mart__player_analysis.sql
@@ -16,26 +16,27 @@ teams as (
     select * from {{ ref('stg__teams')}}
 )
 
+
 select
-    squads.team_id,
-    squads.player_id,
-    squads.season_year,
+    squads.team_id          as team_id,
+    squads.player_id        as player_id,
+    squads.season_year      as season_year,
 
-    squads.position_name,
-    squads.jersey_number,
-    players.player_name,
+    squads.position_name    as position_name,
+    squads.jersey_number    as jersey,
+    players.player_name     as player_name,
 
-    players.nationality,
-    players.gender,
+    players.nationality     as nationality,
+    players.gender          as gender,
 
-    teams.team_name,
-    teams.team_code,
-    teams.city as team_city,
+    teams.team_name         as team_name,
+    teams.team_code         as team_code,
+    teams.city              as team_city,
 
-    players.height_cm,
-    players.weight_kg,
+    players.height_cm       as height_cm,
+    players.weight_kg       as weight_kg,
 
-    players.birth_date,
+    players.birth_date      as birth_date,
     date_diff('year', players.birth_date, current_date) as current_age,
 
 from
