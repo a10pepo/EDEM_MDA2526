fe2de71dc6b62687186fdbd5253d022c2a1d7bb0
Javier Plaza Rosique <j.plazarosique@gmail.com>
2025-12-15 23:46:23 +0100

    segir con get_api

--- ALUMNOS/MDAA/JAVIER_PLAZA/APIS/app_get_api/app_get_api.py
+++ ALUMNOS/MDAA/JAVIER_PLAZA/APIS/app_get_api/app_get_api.py
@@ -6,17 +6,19 @@ import requests
 
 url_frutas = "https://fruityvice.com/api/fruit/all"
 
-# 
+# Llamada a la API con todas las frutas para poder sacar las frutas que existen.
 respuesta_url_frutas = request(url_frutas)
 
 respuesta_url_frutas = respuesta_url_frutas.json()
 
+# Creación de la lista de las frutas y bucle para introducir las frutas en la lista.
 frutas = []
-
 for fruta in respuesta_url_frutas["name"]:
     frutas.append(fruta)
 
+# Creación de la aplicación donde se encuentra la API
 app = Flask(__name__)
 auth = HTTPBasicAuth()
 
-app.config['JWT_SECRET_KEY']
+app.config['JWT_SECRET_KEY'] = 'your_secret_jwt_key'
+jwt = JWTManager(app)
