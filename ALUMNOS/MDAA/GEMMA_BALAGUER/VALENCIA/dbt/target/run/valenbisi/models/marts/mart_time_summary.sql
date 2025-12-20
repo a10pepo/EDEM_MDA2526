b8f41058e9ee3f9af0d4f2c4d83bac9bde763c89
gemmabalagueer <gbalagueradell@gmail.com>
2025-12-20 16:34:02 +0100

    Valenbisi gemma (#430)
    * commit
    
    * commit
    
    * visualizacion
    
    solo  faltaconectar a base de datos no relacional

---
+++ ALUMNOS/MDAA/GEMMA_BALAGUER/VALENCIA/dbt/target/run/valenbisi/models/marts/mart_time_summary.sql
@@ -0,0 +1,23 @@
+
+  
+    
+
+  create  table "pruebadb"."public"."mart_time_summary__dbt_tmp"
+  
+  
+    as
+  
+  (
+    
+
+SELECT
+    date,
+    SUM(avg_available) AS total_available,
+    SUM(avg_free) AS total_free,
+    SUM(avg_total) AS total_capacity,
+    ROUND(AVG(avg_occupancy_pct), 2) AS avg_occupancy_pct
+FROM "pruebadb"."public"."int_valenbisi_time"
+GROUP BY date
+ORDER BY date
+  );
+  
