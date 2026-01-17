c828edca233c0fb964f7dcaee63b3bb2403598af
riedpe <riedpe@edem.es>
2026-01-17 13:08:54 +0100

    Update consumer.py

--- PROFESORES/MDA/KAFKA/Ejercicios/ejercicio_4_kafka_consumer_python_app/consumer.py
+++ PROFESORES/MDA/KAFKA/Ejercicios/ejercicio_4_kafka_consumer_python_app/consumer.py
@@ -6,14 +6,14 @@ from confluent_kafka import Consumer
 config = {
     'bootstrap.servers': 'localhost:9092',  # Dirección del broker Kafka (como la IP de un servicio web)
     'group.id': 'grupo-consumidor',         # Identificador del grupo de consumidores
-    'auto.offset.reset': 'latest'         # Leer desde el principio si no hay posición guardada
+    'auto.offset.reset': 'earliest'         # Leer desde el principio si no hay posición guardada
 }
 
 # Creamos el consumidor con la configuración anterior
 consumer = Consumer(config)
 
 # Nos suscribimos al tópico que queremos leer
-topic_kafka ='proyectos-empresariales'
+topic_kafka ='proyectos_innovacion'
 consumer.subscribe([topic_kafka])
 
 
@@ -41,7 +41,7 @@ try:
         # ¿Por qué 1 segundo?
         #   - Es un buen equilibrio: no bloquea demasiado tiempo y no consume recursos en exceso.
         # ============================================
-        msg = consumer.poll(0.5)
+        msg = consumer.poll(1.0)
 
         if msg is None:
             # No hay mensaje disponible en este momento, seguimos esperando
@@ -51,9 +51,6 @@ try:
             # Si hay un error en el mensaje, lo mostramos
             print(f"Error al recibir mensaje: {msg.error()}")
             continue
-        v = msg.value().decode('utf-8')
-        print(f"Mensaje recibido: {v}")
-        print(type(v))
         # Si el mensaje es válido, mostramos su contenido
         # msg.value() devuelve los datos en bytes, por eso usamos decode('utf-8') para convertirlos a texto
         print(f"Mensaje recibido: {msg.value().decode('utf-8')}")
