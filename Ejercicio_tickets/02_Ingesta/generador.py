cad55607edddaf7c0058c96555093ff1189f9fe6
alvarogimenezc <alvarogimenezc24@gmail.com>
2026-02-06 17:28:15 +0100

    c
    c

--- Ejercicio_tickets/02_Ingesta/generador.py
+++ Ejercicio_tickets/02_Ingesta/generador.py
@@ -10,7 +10,7 @@ while True:
     tienda= ["Recambios Moratalla", "Tienda de Electrónica", "Supermercado El Ahorro", "Librería El Saber", "Ropa y Moda"]
     direcciones= ["Calle Falsa 123", "Avenida Siempre Viva 456", "Plaza Mayor 789", "Calle del Sol 321", "Avenida de la Luna 654"]
     fechas=["2024-06-01", "2024-06-02", "2024-06-03", "2024-06-04", "2024-06-05"]
-    producto["Producto A", "Producto B", "Producto C", "Producto D", "Producto E"]
+    producto=["Producto A", "Producto B", "Producto C", "Producto D", "Producto E"]
 
     aleatorio=random.randint(0, 4)
 
