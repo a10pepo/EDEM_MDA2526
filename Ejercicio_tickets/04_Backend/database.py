57a8761bdf926bd70057b13c804c3cb3d633ebff
Miguel Ángel Navarro <miguelanav01@gmail.com>
2026-02-06 18:28:03 +0100

    Backend & database python

--- Ejercicio_tickets/04_Backend/database.py
+++ Ejercicio_tickets/04_Backend/database.py
@@ -5,110 +5,6 @@ import pandas as pd
 import os
 from pathlib import Path
 
-# Diccionario con los metadatos de cada estación (basado en la API de Valencia)
-# La clave es el objectid (nombre del archivo CSV)
-
-# STATIONS_METADATA = {
-#     12: {
-#         "nombre": "Dr. Lluch",
-#         "direccion": "DR.LLUCH",
-#         "tipozona": "Urbana",
-#         "tipoemisio": "Tráfico",
-#         "fiwareid": "A08_DR_LLUCH_60m",
-#         "geo_shape": {"type": "Feature", "geometry": {"coordinates": [-0.328289489402739, 39.4666847554611], "type": "Point"}, "properties": {}},
-#         "geo_point_2d": {"lon": -0.328289489402739, "lat": 39.4666847554611}
-#     },
-#     13: {
-#         "nombre": "Francia",
-#         "direccion": "AVDA.FRANCIA",
-#         "tipozona": "Urbana",
-#         "tipoemisio": "Tráfico",
-#         "fiwareid": "A01_AVFRANCIA_60m",
-#         "geo_shape": {"type": "Feature", "geometry": {"coordinates": [-0.342986232422652, 39.4578268875183], "type": "Point"}, "properties": {}},
-#         "geo_point_2d": {"lon": -0.342986232422652, "lat": 39.4578268875183}
-#     },
-#     14: {
-#         "nombre": "Boulevar Sur",
-#         "direccion": "BULEVARD SUD",
-#         "tipozona": "Urbana",
-#         "tipoemisio": "Tráfico",
-#         "fiwareid": "A02_BULEVARDSUD_60m",
-#         "geo_shape": {"type": "Feature", "geometry": {"coordinates": [-0.396337564375856, 39.4503960055054], "type": "Point"}, "properties": {}},
-#         "geo_point_2d": {"lon": -0.396337564375856, "lat": 39.4503960055054}
-#     },
-#     15: {
-#         "nombre": "Molí del Sol",
-#         "direccion": "MOLÍ DEL SOL",
-#         "tipozona": "Suburbana",
-#         "tipoemisio": "Tráfico",
-#         "fiwareid": "A03_MOLISOL_60m",
-#         "geo_shape": {"type": "Feature", "geometry": {"coordinates": [-0.408809896900938, 39.4811121109041], "type": "Point"}, "properties": {}},
-#         "geo_point_2d": {"lon": -0.408809896900938, "lat": 39.4811121109041}
-#     },
-#     16: {
-#         "nombre": "Pista de Silla",
-#         "direccion": "PISTA DE SILLA",
-#         "tipozona": "Urbana",
-#         "tipoemisio": "Tráfico",
-#         "fiwareid": "A04_PISTASILLA_60m",
-#         "geo_shape": {"type": "Feature", "geometry": {"coordinates": [-0.376643936579157, 39.4580609536967], "type": "Point"}, "properties": {}},
-#         "geo_point_2d": {"lon": -0.376643936579157, "lat": 39.4580609536967}
-#     },
-#     17: {
-#         "nombre": "Universidad Politécnica",
-#         "direccion": "POLITÈCNIC",
-#         "tipozona": "Suburbana",
-#         "tipoemisio": "Fondo",
-#         "fiwareid": "A05_POLITECNIC_60m",
-#         "geo_shape": {"type": "Feature", "geometry": {"coordinates": [-0.337400660521869, 39.4796444969292], "type": "Point"}, "properties": {}},
-#         "geo_point_2d": {"lon": -0.337400660521869, "lat": 39.4796444969292}
-#     },
-#     18: {
-#         "nombre": "Viveros",
-#         "direccion": "VIVERS",
-#         "tipozona": "Urbana",
-#         "tipoemisio": "Fondo",
-#         "fiwareid": "A06_VIVERS_60m",
-#         "geo_shape": {"type": "Feature", "geometry": {"coordinates": [-0.36964822314381, 39.4796409248053], "type": "Point"}, "properties": {}},
-#         "geo_point_2d": {"lon": -0.36964822314381, "lat": 39.4796409248053}
-#     },
-#     19: {
-#         "nombre": "Centro",
-#         "direccion": "VALÈNCIA CENTRE",
-#         "tipozona": "Urbana",
-#         "tipoemisio": "Tráfico",
-#         "fiwareid": "A07_VALENCIACENTRE_60m",
-#         "geo_shape": {"type": "Feature", "geometry": {"coordinates": [-0.376397651655324, 39.4705476702601], "type": "Point"}, "properties": {}},
-#         "geo_point_2d": {"lon": -0.376397651655324, "lat": 39.4705476702601}
-#     },
-#     20: {
-#         "nombre": "Cabanyal",
-#         "direccion": "CABANYAL",
-#         "tipozona": "Urbana",
-#         "tipoemisio": "Fondo",
-#         "fiwareid": "A09_CABANYAL_60m",
-#         "geo_shape": {"type": "Feature", "geometry": {"coordinates": [-0.328534813492744, 39.4743907853568], "type": "Point"}, "properties": {}},
-#         "geo_point_2d": {"lon": -0.328534813492744, "lat": 39.4743907853568}
-#     },
-#     21: {
-#         "nombre": "Olivereta",
-#         "direccion": "OLIVERETA",
-#         "tipozona": "Urbana",
-#         "tipoemisio": "Tráfico",
-#         "fiwareid": "A10_OLIVERETA_60m",
-#         "geo_shape": {"type": "Feature", "geometry": {"coordinates": [-0.405923445529068, 39.469244235092], "type": "Point"}, "properties": {}},
-#         "geo_point_2d": {"lon": -0.405923445529068, "lat": 39.469244235092}
-#     },
-#     22: {
-#         "nombre": "Patraix",
-#         "direccion": "PATRAIX",
-#         "tipozona": "Urbana",
-#         "tipoemisio": "Tráfico",
-#         "fiwareid": "A11_PATRAIX_60m",
-#         "geo_shape": {"type": "Feature", "geometry": {"coordinates": [-0.401411329219129, 39.4591890899964], "type": "Point"}, "properties": {}},
-#         "geo_point_2d": {"lon": -0.401411329219129, "lat": 39.4591890899964}
-#     },
-# }
 
 def init_db():
     """Inicializa la infraestructura de la base de datos (esquemas y tablas)."""
@@ -126,19 +22,24 @@ def init_db():
                 # conn.execute(text("CREATE SCHEMA IF NOT EXISTS intermediate;"))
                 # conn.execute(text("CREATE SCHEMA IF NOT EXISTS marts;"))
 
-                # 2. Tabla para Valencia (datos en tiempo real de la API)
+                # 2. Tabla de registro de tickets (datos de la API)
                 conn.execute(text("""
                     CREATE TABLE IF NOT EXISTS raw.tickets (
-                        id_ticket SERIAL PRIMARY KEY,
-                        purchase_date TIMESTAMPTZ,
-                        price NUMERIC(10,2),
-                        shop VARCHAR,
-                        UNIQUE(id_ticket)
+                        ticket_id          INT PRIMARY KEY,
+                        timestamp       TIMESTAMPTZ NOT NULL,
+                        adress VARCHAR,
+                        shop_name VARCHAR,
+                        latitud NUMERIC(9,6),
+                        longitud NUMERIC(9,6),
+                        product_name VARCHAR,
+                        import NUMERIC(10,2), -- Renamed from 'price' to match the dict key
+                        refund_deadline TIMESTAMPTZ,
+                        change_deadline TIMESTAMPTZ
                     );
                 """))
                 conn.commit()
                 print("✅ Base de datos lista: Esquemas y tablas RAW creados correctamente.")
-                return 
+                return
 
         except Exception as e:
             print(f"⚠️ Intento {i+1} fallido: {e}")
--- Ejercicio_tickets/04_Backend/main.py
+++ Ejercicio_tickets/04_Backend/main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI, HTTPException, Depends, Security, Query
 from fastapi.security import APIKeyHeader
-from config import engine
+from config_env import *
 import pandas as pd
 from pydantic import BaseModel, ConfigDict
 from typing import Optional, Dict, Any
@@ -10,73 +10,17 @@ from database import init_db, load_historical_real_data, load_historical_simulat
 from sqlalchemy.dialects.postgresql import insert
 import math
 
-
 # ----------------------------------
-
-# Clase principal de la medición
-class AirQualityInbound(BaseModel):
-    # Identificadores (Obligatorios)
-    objectid: int
-    fiwareid: str
-    nombre: str
-    direccion: str
-    
-    # Contexto de la zona
-    tipozona: str
-    tipoemisio: str
-    calidad_am: str
-    fecha_carg: str
-    
-    # Parámetros descriptivos (Pueden ser nulos en el JSON)
-    parametros: Optional[str] = None
-    mediciones: Optional[str] = None
-
-    # Mediciones de Contaminantes (Opcionales para evitar errores si falta alguno)
-    so2: Optional[float] = None
-    no2: Optional[float] = None
-    o3: Optional[float] = None
-    co: Optional[float] = None
-    pm10: Optional[float] = None
-    pm25: Optional[float] = None
-
-    # Geografía: Definidos como diccionarios genéricos por ahora
-    # Solo validamos que sea un diccionario, no miramos qué hay dentro.
-
-    geo_shape: Dict[str, Any]   # Le decimos a pylance que la clave del diccionario debe ser string pero el valor asociado a la clave puede ser cualquiera
-    geo_point_2d: Dict[str, Any]
-
-    # CONFIGURACIÓN DE SEGURIDAD
-    # 'forbid' asegura que no aceptamos ningún campo nuevo que no esté en esta lista
-
-    model_config = ConfigDict(extra='forbid')
-
-# ----------------------------------
-
-@asynccontextmanager    # El decorador es un envoltorio funcional. Le dice a python que la función es un Gestor de Contexto (Context Manager) y tiene dos tiempos, una al arrancar (Antes del yield) y otra al apagar la api (Despues del yield)
-async def lifespan(app: FastAPI):
-
-    # --- CÓDIGO AL ARRANCAR EL CONTENEDOR ---
-
-    try:
-        init_db() # Crea los esquemas y tablas necesarias.
-
-        # Cargar datos históricos (solo se ejecuta si la tabla está vacía)
-        
-    except Exception as e:
-        print(f"❌ Error inicializando la BD: {e}")
-    yield   #Pausa la ejecución de la función para seguir con la aplicación.
-            #Se pueden configurar acciones a realizar al apagar la api
 
 # Inicialización de la API
 app = FastAPI(
     lifespan=lifespan,
-    title="Air Quality Barrier API",
-    description="API de aislamiento para proteger el acceso a air_quality_db",
+    title="Tickets security API",
+    description="API de aislamiento para proteger el acceso a {POSTGRES_DB}",
     version="1.0.0"
 )
 
 # ----------------------------------
-
 
 # --- ENDPOINTS ---
 
@@ -96,15 +40,18 @@ async def health_check():
 
 # --- ENDPOINTS INGESTA ---
 
-@app.post("/tickets/", status_code=201)
+@app.post("/ingestion/", status_code=201)
 async def create_ticket(ticket: TicketCreate):
     """
     Endpoint to insert a new ticket record into the raw.tickets table.
     """
     query = text("""
-        INSERT INTO raw.tickets (purchase_date, price, shop)
-        VALUES (:purchase_date, :price, :shop)
-        RETURNING id_ticket;
+        INSERT INTO raw.tickets (
+            ticket_id, timestamp, adress, shop_name, latitud, longitud, product_name, import, refund_deadline, change_deadline)
+        VALUES (
+            :ticket_id, :timestamp, :adress, :shop_name, :latitud, :longitud, :product_name, :import, :refund_deadline, :change_deadline
+        )
+        ON CONFLICT (ticket_id) DO NOTHING;
     """)
     
     try:
@@ -125,7 +72,7 @@ async def create_ticket(ticket: TicketCreate):
             "message": "Ticket created successfully",
             "id_ticket": new_id
         }
-        
+
     except Exception as e:
         # Log the error and return a 500 status code
         print(f"Database error: {e}")
