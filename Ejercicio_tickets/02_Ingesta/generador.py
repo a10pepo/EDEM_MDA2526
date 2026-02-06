f654e5ad0d87ecbdc698c1ed637323ff83a33070
alvarogimenezc <alvarogimenezc24@gmail.com>
2026-02-06 17:04:41 +0100

    c
    c

--- Ejercicio_tickets/02_Ingesta/generador.py
+++ Ejercicio_tickets/02_Ingesta/generador.py
@@ -10,6 +10,7 @@ while True:
     tienda= ["Recambios Moratalla", "Tienda de Electrónica", "Supermercado El Ahorro", "Librería El Saber", "Ropa y Moda"]
     direcciones= ["Calle Falsa 123", "Avenida Siempre Viva 456", "Plaza Mayor 789", "Calle del Sol 321", "Avenida de la Luna 654"]
     fechas=["2024-06-01", "2024-06-02", "2024-06-03", "2024-06-04", "2024-06-05"]
+    producto["Producto A", "Producto B", "Producto C", "Producto D", "Producto E"]
 
     aleatorio=random.randint(0, 4)
  
@@ -20,6 +21,7 @@ while True:
         "timestamp": "2024-06-01 11:00:00",
         "adress": direcciones[aleatorio],
         "shop_name": tienda[aleatorio], 
+        "product_name": producto[random.randint(0, 4)],
         "import": round(random.uniform(10.0, 1000.0), 2),
         "category": 
         "refund_deadline": fechas[random.randint(0, 4)],
