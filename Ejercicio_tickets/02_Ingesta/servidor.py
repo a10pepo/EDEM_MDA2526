5dba4fc83c14160ad4722eec4af3141c61999f0a
alvarogimenezc <alvarogimenezc24@gmail.com>
2026-02-06 16:25:16 +0100

    c
    c

---
+++ Ejercicio_tickets/02_Ingesta/servidor.py
@@ -0,0 +1,14 @@
+import flask 
+
+#Creamos la instancia de aplicación (levantamos el servidor)
+app = flask.Flask(__name__)
+
+#Ruta1
+@app.route('/ingestion', methods=['POST'])
+def ruta1():
+    a=1
+
+
+
+
+    
