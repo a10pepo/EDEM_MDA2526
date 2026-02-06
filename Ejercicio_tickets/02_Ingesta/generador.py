786194f48df85c349ba02b1d1a8bce597dc85c9b
alvarogimenezc <alvarogimenezc24@gmail.com>
2026-02-06 16:57:43 +0100

    c
    c

--- Ejercicio_tickets/02_Ingesta/generador.py
+++ Ejercicio_tickets/02_Ingesta/generador.py
@@ -12,18 +12,25 @@ while True:
     fechas=["2024-06-01", "2024-06-02", "2024-06-03", "2024-06-04", "2024-06-05"]
 
     aleatorio=random.randint(0, 4)
+ 
+    #Hay que hacer un get a la bbdd para saber de que categoría es la tienda
+    #categoria=["pepe"]
 
     ticket= {"ticket_id": random.randint(1, 1000),
         "timestamp": "2024-06-01 11:00:00",
         "adress": direcciones[aleatorio],
         "shop_name": tienda[aleatorio], 
         "import": round(random.uniform(10.0, 1000.0), 2),
+        "category": 
         "refund_deadline": fechas[random.randint(0, 4)],
         "change_deadline": fechas[random.randint(0, 4)]
         }
 
     #Llamamos a la ruta del servidor para enviar el ticket
-    response = requests.post(FLASK_API_URL, json={"ticket": ticket})
-        
+    try:
+      response = requests.post(FLASK_API_URL, json={"ticket": ticket})
+    except:
+      print("Error al enviar el ticket. El servidor no está disponible.")
+
     #Esperamos 200 segundos hasta el nuevo ticket 
-    sleep(200)
+    sleep(20)
