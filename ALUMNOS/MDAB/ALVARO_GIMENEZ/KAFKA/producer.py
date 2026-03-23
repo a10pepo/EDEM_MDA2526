0a857ee66766027eb70fd70a2261f5f6e8514aad
alvarogimenezc <alvarogimenezc24@gmail.com>
2026-03-23 21:28:31 +0100

    c

--- ALUMNOS/MDAB/ALVARO_GIMENEZ/KAFKA/producer.py
+++ ALUMNOS/MDAB/ALVARO_GIMENEZ/KAFKA/producer.py
@@ -7,19 +7,19 @@ import random
 import json
 from kafka import KafkaProducer
 
-#Configuramos el objeto producer para enviar los mensaje
+#Configuramos el objeto producer para enviar los mensajes
 producer = KafkaProducer(
     bootstrap_servers=['localhost:9092'],
     value_serializer=lambda x: json.dumps(x).encode('utf-8')
 )
 
 #Generamos un array (lista) de clientes y referencias para la simulación
-clientes=["Cliente 1", "Cliente 2", "Cliente 3", "Cliente 4", "Cliente 5", "Cliente 6", "Cliente 7", "Cliente 8", "Cliente 9", "Cliente 10"]
+clientes=["Customer 1", "Customer 2", "Customer 3", "Customer 4", "Customer 5", "Customer 6", "Customer 7", "Customer 8", "Customer 9", "Customer 10"]
 referencias=["REF-001", "REF-002", "REF-003", "REF-004", "REF-005", "REF-006", "REF-007", "REF-008", "REF-009", "REF-010"]
 
 #Creamos el generador de logs
-def generador_entregas(): 
-     while True:
+def generador_entregas(order_id): 
+
         cliente = random.choice(clientes)
         referencia=random.choice(referencias)
         cantidad_pedido = random.randint(1, 100)
@@ -32,8 +32,44 @@ def generador_entregas():
             cantidad_servida = cantidad_pedido
 
         #Generamos el paquete de datos a enviar (log simulado)
-        log_entrega={"cliente": cliente, 
-        "referencia": referencia,
-        "cantidad_pedido", cantidad_pedido,
-        "cantidad_servida", cantidad_servida
-        "Diferencia", cantidad_pedido - cantidad_servida}
+        log_entrega={"timestamp": time.time(),
+        "order_id": order_id,
+        "customer": cliente, 
+        "sku": referencia,
+        "order_qnt": cantidad_pedido,
+        "served_qnt": cantidad_servida,
+        "diff": cantidad_pedido - cantidad_servida}
+
+        return log_entrega
+
+#Ejecutor principal (no sería necesario, no vamos a reutilizar las funciones, pero es buena práctica)
+if __name__ == "__main__":
+
+    #Contador para los order-id, en cada ejecución del servicio se reiniciará¡! Ojo consistencia de ids si escalamos funcionalidad
+    contador=0
+
+    while True: 
+
+        try:
+            order_id = f"ORD-{contador}"
+            log_entrega = generador_entregas(order_id)
+
+            #Mandamos el mensaje (log de simulación) al tópico de KAFKA
+            producer.send('entregas', value=log_entrega)
+
+            #Sacamos un log resumen por pantalla
+            print(f"Log enviado: {log_entrega}")
+
+            #Aumentamos el contador
+            contador += 1
+    
+            #Dormimos medio segundo hasta el próximo log
+            time.sleep(0.5)
+
+        #Manejamos los posibles errores que pueden darse
+        except KeyboardInterrupt:
+            print("Simulación detenida por el usuario.")
+        except Exception as e:
+            print(f"Error inesperado: {e}")
+        finally:
+            producer.close()
